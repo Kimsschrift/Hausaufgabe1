80baf9d45bddebca88a4f136d9686379163a10e8
Dongwoo Kim <kyle021581@gmail.com>
2024-10-31 20:45:35 +0100

    Die endgültige Version von Calculator.java

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -14,6 +14,10 @@ public class Calculator {
 
     private String latestOperation = "";
 
+    private int clearCount = 0;
+
+    private int binaryOperationCount = 0;
+
     /**
      * @return den aktuellen Bildschirminhalt als String
      */
@@ -28,11 +32,15 @@ public class Calculator {
      * oder rechts an die zuvor gedrückte Ziffer angehängt angezeigt wird.
      * @param digit Die Ziffer, deren Taste gedrückt wurde
      */
+
     public void pressDigitKey(int digit) {
         if(digit > 9 || digit < 0) throw new IllegalArgumentException();
 
-        if(screen.equals("0") || latestValue == Double.parseDouble(screen)) screen = "";
+        if (screen.contains(".") && binaryOperationCount == 0) {
 
+        } else if (screen.equals("0") || latestValue == Double.parseDouble(screen)){
+            screen = "";
+        }
         screen = screen + digit;
     }
 
@@ -45,9 +53,15 @@ public class Calculator {
      * im Ursprungszustand ist.
      */
     public void pressClearKey() {
-        screen = "0";
-        latestOperation = "";
-        latestValue = 0.0;
+        clearCount++;
+        if(clearCount == 1) {
+            screen = "0";
+        } else {
+            screen = "0";
+            latestOperation = "";
+            latestValue = 0.0;
+            clearCount = 0;
+        }
     }
 
     /**
@@ -60,8 +74,15 @@ public class Calculator {
      * @param operation "+" für Addition, "-" für Substraktion, "x" für Multiplikation, "/" für Division
      */
     public void pressBinaryOperationKey(String operation)  {
+
+
+        if (binaryOperationCount != 0){
+            pressEqualsKey();
+        }
+
         latestValue = Double.parseDouble(screen);
         latestOperation = operation;
+        binaryOperationCount++;
     }
 
     /**
@@ -118,16 +139,22 @@ public class Calculator {
      * und das Ergebnis direkt angezeigt.
      */
     public void pressEqualsKey() {
+        if (latestOperation.isEmpty()) return;
+
+        var currentValue = Double.parseDouble(screen);
+
         var result = switch(latestOperation) {
-            case "+" -> latestValue + Double.parseDouble(screen);
-            case "-" -> latestValue - Double.parseDouble(screen);
-            case "x" -> latestValue * Double.parseDouble(screen);
-            case "/" -> latestValue / Double.parseDouble(screen);
+            case "+" -> latestValue + currentValue;
+            case "-" -> latestValue - currentValue;
+            case "x" -> latestValue * currentValue;
+            case "/" -> latestValue / currentValue;
             default -> throw new IllegalArgumentException();
         };
         screen = Double.toString(result);
         if(screen.equals("Infinity")) screen = "Error";
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+
+        latestValue = currentValue;
     }
 }
